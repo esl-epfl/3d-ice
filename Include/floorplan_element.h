28dec5419dac558abd495e719efff76391e11c9c
vincenzi <vincenzi@97db0c5e-453e-47ca-9a6c-32dc3a2cf244>
2010-05-03 15:55:39 +0000

    Added FloorplanElement data type sources

---
+++ Include/floorplan_element.h
@@ -0,0 +1,85 @@
+/******************************************************************************
+ *                                                                            *
+ * Header file "Include/floorplan_element.h"                                  *
+ *                                                                            *
+ * EPFL-STI-IEL-ESL                                                           *
+ * Bâtiment ELG, ELG 130                                                      *
+ * Station 11                                                                 *
+ * 1015 Lausanne, Switzerland                    alessandro.vincenzi@epfl.ch  *
+ ******************************************************************************/
+
+#ifndef _TL_FLOORPLAN_ELEMENT_H_
+#define _TL_FLOORPLAN_ELEMENT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdio.h>
+
+/******************************************************************************
+ *                                                                            *
+ * "FloorplanElement" : the representation of a single element composing a    *
+ *                      floorplan.                                            *
+ *                                                                            *
+ ******************************************************************************/
+
+  struct floorplan_element
+  {
+    char *Id ; /* The id (string) of the floorplan element */
+
+    int SW_X ;           /* The south-west corner X position (um) */
+    int SW_Y ;           /* The south-west corner Y position (um) */
+
+    int Length ;         /* The length of the floorplan element (um) */
+    int Width ;          /* The width of the floorplan element (um) */
+
+    int SW_Row ;         /* The row index corresponding to the south-west */
+                         /* corner                                        */
+
+    int SW_Column ;      /* The column index corresponding to the south-west */
+                         /* corner                                           */
+
+    int NE_Row ;         /* The row index corresponding to the north-east */
+                         /* corner                                        */
+
+    int NE_Column ;      /* The column index corresponding to the north-east */
+                         /* corner                                           */
+
+    double PowerValue ;  /* The current power value */
+
+    struct floorplan_element *Next ; /* To collect floorplan elements */
+                                     /* in a linked list              */
+
+  } ;
+
+  typedef struct floorplan_element FloorplanElement ;
+
+/******************************************************************************/
+
+  void init_floorplan_element(FloorplanElement *floorplan_element) ;
+
+  FloorplanElement *malloc_and_init_floorplan_element (void) ;
+
+  void free_floorplan_element  (FloorplanElement *floorplan_element) ;
+
+  void free_floorplan_elements_list (FloorplanElement *list) ;
+
+  void print_floorplan_element (FILE *stream,
+                                char *prefix,
+                                FloorplanElement *floorplan_element) ;
+
+  void print_floorplan_elements_list (FILE *stream,
+                                      char *prefix,
+                                      FloorplanElement *list) ;
+
+  int check_intersection (FloorplanElement *floorplan_element_a,
+                          FloorplanElement *floorplan_element_b) ;
+
+/******************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _TL_FLOORPLAN_ELEMENT_H_ */
--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ LIB            = Lib/libTermalLibrary.a
 
 OBJECTS = Sources/dimensions.o          \
           Sources/material.o            \
-          Sources/layer.o
+          Sources/layer.o               \
+          Sources/floorplan_element.o
 
 all: $(LIB)
 
@@ -32,6 +33,12 @@ Sources/material.o: Sources/material.c Include/material.h
 ################################################################################
 
 Sources/layer.o: Sources/layer.c Include/layer.h
+	$(CC) $(CFLAGS) -o $@ $(INCLUDE) -c $<
+
+################################################################################
+
+Sources/floorplan_element.o: Sources/floorplan_element.c \
+                             Include/floorplan_element.h
 	$(CC) $(CFLAGS) -o $@ $(INCLUDE) -c $<
 
 ################################################################################
