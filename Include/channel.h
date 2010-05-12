8a5a11e286ace979258195e16ad13d7b2c330c4c
vincenzi <vincenzi@97db0c5e-453e-47ca-9a6c-32dc3a2cf244>
2010-05-12 13:04:45 +0000

    Fixed channel header and source file

--- Include/channel.h
+++ Include/channel.h
@@ -23,79 +23,93 @@ extern "C"
 
 /******************************************************************************
  *                                                                            *
- * "Channel" : the representation of a single chennel.                        *
+ * "Channel" : the representation of a single channel.                        *
  *                                                                            *
  ******************************************************************************/
 
   typedef struct
   {
-    double Height ;          /* The heigh of the channel in um */
-                             /* (corresponding to 1 cell).     */
+    double Height ;           /* The heigh of the channel in um               */
+                              /* (corresponding to 1 cell).                   */
 
-    double LiquidHTC ;       /* The heat transfert coefficent of */
-                             /* the cooling liquid               */
+    double LiquidHTC ;        /* The heat transfert coefficent of             */
+                              /* the cooling liquid                           */
 
-    double LiquidSH ;        /* The specific heat of the cooling liquid */
+    double LiquidSH ;         /* The specific heat of the cooling liquid      */
 
-    Material *WallMaterial ; /* A pointer to the Material composing the wall */
+    Material *WallMaterial ;  /* A pointer to the Material composing the wall */
 
   } Channel ;
 
 /******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
 
-  void init_channel  (Channel *channel) ;
+  void
+  init_channel               (Channel *channel) ;
 
-  Channel *alloc_and_init_channel (void) ;
+  Channel *
+  alloc_and_init_channel     (void) ;
 
-  void free_channel (Channel *channel) ;
+  void
+  free_channel               (Channel *channel) ;
 
-  void print_channel (FILE *stream, char *prefix, Channel *channel) ;
+  void
+  print_channel              (FILE *stream, char *prefix, Channel *channel) ;
 
-  Resistances *fill_resistances_channel (
-#ifdef DEBUG_FILL_RESISTANCES
-                                         FILE *debug,
-#endif
-                                         Channel *channel,
-                                         Resistances *resistances,
-                                         Dimensions *dim,
-                                         int current_layer) ;
-
-
-  double *fill_capacities_channel (
-#ifdef DEBUG_FILL_CAPACITIES
-                                    FILE *debug,
-                                    int current_layer,
-#endif
-                                    Channel *channel,
-                                    double *capacities,
-                                    Dimensions *dim,
-                                    double delta_time) ;
-
-  double *fill_sources_channel (
-#ifdef DEBUG_FILL_SOURCES
-                                FILE *debug,
-                                int current_layer,
-#endif
-                                Channel *channel,
-                                double *sources,
-                                Dimensions *dim) ;
-
-  int fill_system_matrix_channel
-                (
-#ifdef DEBUG_FILL_SYSTEM_MATRIX
-                  FILE *debug,
-                  Channel *channel,
-#endif
-                  Dimensions *dim,
-                  Resistances *resistances,
-                  double *capacities,
-                  int *columns,
-                  int *rows,
-                  double *values,
-                  int current_layer
-                ) ;
+  Resistances *
+  fill_resistances_channel   (
+                              #ifdef DEBUG_FILL_RESISTANCES
+                              FILE        *debug,
+                              #endif
+                              Channel     *channel,
+                              Resistances *resistances,
+                              Dimensions  *dim,
+                              int         current_layer
+                             ) ;
 
 
+  double *
+  fill_capacities_channel    (
+                              #ifdef DEBUG_FILL_CAPACITIES
+                              FILE       *debug,
+                              int        current_layer,
+                              #endif
+                              Channel    *channel,
+                              double     *capacities,
+                              Dimensions *dimensions,
+                              double     delta_time
+                             ) ;
+
+  double *
+  fill_sources_channel       (
+                              #ifdef DEBUG_FILL_SOURCES
+                              FILE       *debug,
+                              int        current_layer,
+                              #endif
+                              Channel    *channel,
+                              double     *sources,
+                              Dimensions *dim
+                             ) ;
+
+  int
+  fill_system_matrix_channel (
+                              #ifdef DEBUG_FILL_SYSTEM_MATRIX
+                              FILE        *debug,
+                              Channel     *channel,
+                              #endif
+                              Dimensions  *dimensions,
+                              Resistances *resistances,
+                              double      *capacities,
+                              int         *columns,
+                              int         *rows,
+                              double      *values,
+                              int         current_layer
+                             ) ;
+
+
+/******************************************************************************/
+/******************************************************************************/
 /******************************************************************************/
 
 #ifdef __cplusplus
--- Sources/channel.c
+++ Sources/channel.c
@@ -13,14 +13,19 @@
 #include "channel.h"
 #include "system_matrix.h"
 
-void
-init_channel (Channel* channel)
-{
-  if (channel == NULL) return ;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
 
-  channel->Height       = 0.0 ;
-  channel->LiquidHTC    = 0.0 ;
-  channel->LiquidSH     = 0.0 ;
+void
+init_channel
+(
+  Channel* channel
+)
+{
+  channel->Height       = 0.0  ;
+  channel->LiquidHTC    = 0.0  ;
+  channel->LiquidSH     = 0.0  ;
   channel->WallMaterial = NULL ;
 }
 
@@ -29,11 +34,14 @@ init_channel (Channel* channel)
 /******************************************************************************/
 
 Channel *
-alloc_and_init_channel (void)
+alloc_and_init_channel
+(
+  void
+)
 {
   Channel *channel = (Channel *) malloc ( sizeof(Channel) ) ;
 
-  init_channel (channel) ;
+  if (channel != NULL) init_channel (channel) ;
 
   return channel ;
 }
@@ -43,10 +51,11 @@ alloc_and_init_channel (void)
 /******************************************************************************/
 
 void
-free_channel (Channel *channel)
+free_channel
+(
+  Channel *channel
+)
 {
-  if (channel == NULL) return ;
-
   free (channel) ;
 }
 
@@ -54,27 +63,37 @@ free_channel (Channel *channel)
 /******************************************************************************/
 /******************************************************************************/
 
-void print_channel (FILE *stream, char *prefix, Channel *channel)
+void
+print_channel
+(
+  FILE    *stream,
+  char    *prefix,
+  Channel *channel
+)
 {
-  fprintf(stream, "%sChannel\n", prefix) ;
-  fprintf(stream, "%s  Height                           %5.2f\n",
-    prefix, channel->Height) ;
-  fprintf(stream, "%s  Liquid heat transfert coefficent %.4e\n",
-    prefix, channel->LiquidHTC) ;
-  fprintf(stream, "%s  Liquid specific heat             %.4e\n",
-    prefix, channel->LiquidSH) ;
+  fprintf(stream,
+    "%sChannel\n",                                  prefix) ;
+  fprintf(stream,
+    "%s  Height                           %5.2f\n", prefix, channel->Height) ;
+  fprintf(stream,
+    "%s  Liquid heat transfert coefficent %.5e\n", prefix, channel->LiquidHTC) ;
+  fprintf(stream,
+    "%s  Liquid specific heat             %.4e\n", prefix, channel->LiquidSH) ;
 
   Material *wall = channel->WallMaterial ;
 
   if (wall != NULL)
   {
-    fprintf(stream, "%s  Wall thermal conductivity        %.4e (%s)\n",
+    fprintf(stream,
+      "%s  Wall thermal conductivity        %.4e (%s)\n",
       prefix, wall->ThermalConductivity, wall->Id) ;
-    fprintf(stream, "%s  Wall specific heat               %.4e (%s)\n",
+    fprintf(stream,
+      "%s  Wall specific heat               %.4e (%s)\n",
       prefix, wall->SpecificHeat, wall->Id) ;
   }
   else
-    fprintf(stream, "%s  Wall material not specified\n", prefix) ;
+    fprintf(stream,
+      "%s  Wall material not specified\n", prefix) ;
 }
 
 /******************************************************************************/
@@ -85,12 +104,12 @@ Resistances *
 fill_resistances_channel
 (
 #ifdef DEBUG_FILL_RESISTANCES
-  FILE *debug,
+  FILE        *debug,
 #endif
-  Channel *channel,
+  Channel     *channel,
   Resistances *resistances,
-  Dimensions *dim,
-  int current_layer
+  Dimensions  *dimensions,
+  int         current_layer
 )
 {
   int row, column ;
@@ -101,12 +120,23 @@ fill_resistances_channel
     resistances, current_layer, channel->WallMaterial->Id) ;
 #endif
 
-  for (row = 0 ; row < dim->Grid.NRows ; row++)
-  {
-    for (column = 0 ; column < dim->Grid.NColumns ; column++, resistances++)
-    {
+  for
+  (
+    row = 0 ;
+    row < get_number_of_rows (dimensions) ;
+    row++
+  )
+
+    for
+    (
+      column = 0 ;
+      column < get_number_of_columns (dimensions) ;
+      column++,
+      resistances++
+    )
+
       if (column % 2 == 0) // Even -> wall
-      {
+
         fill_resistances_solid_cell
         (
 #ifdef DEBUG_FILL_RESISTANCES
@@ -115,16 +145,16 @@ fill_resistances_channel
           column,
 #endif
           resistances,
-          dim,
-          get_cell_length (dim, column),
-          dim->Cell.Width,
+          dimensions,
+          get_cell_length (dimensions, column),
+          get_cell_width (dimensions),
           channel->Height,
           channel->WallMaterial->ThermalConductivity,
           current_layer
         ) ;
-      }
+
       else                 // Odd -> channel
-      {
+
         fill_resistances_liquid_cell
         (
 #ifdef DEBUG_FILL_RESISTANCES
@@ -133,17 +163,14 @@ fill_resistances_channel
           column,
 #endif
           resistances,
-          dim,
-          get_cell_length (dim, column),
-          dim->Cell.Width,
+          dimensions,
+          get_cell_length (dimensions, column),
+          get_cell_width (dimensions),
           channel->Height,
           channel->LiquidHTC,
           channel->LiquidSH,
           current_layer
         ) ;
-      }
-    } /* column */
-  } /* row */
 
   return resistances ;
 }
@@ -163,13 +190,13 @@ double *
 fill_capacities_channel
 (
 #ifdef DEBUG_FILL_CAPACITIES
-  FILE *debug,
-  int current_layer,
+  FILE       *debug,
+  int        current_layer,
 #endif
-  Channel *channel,
-  double *capacities,
-  Dimensions *dim,
-  double delta_time
+  Channel    *channel,
+  double     *capacities,
+  Dimensions *dimensions,
+  double     delta_time
 )
 {
   int row, column ;
@@ -180,16 +207,27 @@ fill_capacities_channel
     capacities, current_layer, channel->WallMaterial->Id) ;
 #endif
 
-  for (row = 0 ; row < dim->Grid.NRows ; row++)
-  {
-    for (column = 0 ; column < dim->Grid.NColumns ; column++, capacities++)
-    {
+  for
+  (
+    row = 0 ;
+    row < get_number_of_rows (dimensions) ;
+    row++
+  )
+
+    for
+    (
+      column = 0 ;
+      column < get_number_of_columns (dimensions) ;
+      column++,
+      capacities++
+    )
+
       if (column % 2 == 0 ) /* Even -> Wall */
       {
         *capacities = capacity
                       (
-                        get_cell_length(dim, column),
-                        dim->Cell.Width,
+                        get_cell_length (dimensions, column),
+                        get_cell_width (dimensions),
                         channel->Height,
                         channel->WallMaterial->SpecificHeat,
                         delta_time
@@ -199,7 +237,8 @@ fill_capacities_channel
         "%p wall   cell l %5d r %5d c %5d l %5.2f w %5.2f h %5.2f " \
         "sh %.5e %.5e --> %.5e\n",
         capacities, current_layer, row, column,
-        get_cell_length(dim, column), dim->Cell.Width, channel->Height,
+        get_cell_length(dimensions, column), get_cell_width (dimensions),
+        channel->Height,
         channel->WallMaterial->SpecificHeat, delta_time, *capacities) ;
 #endif
       }
@@ -207,8 +246,8 @@ fill_capacities_channel
       {
         *capacities  = capacity
                        (
-                         get_cell_length(dim, column),
-                         dim->Cell.Width,
+                         get_cell_length(dimensions, column),
+                         get_cell_width (dimensions),
                          channel->Height,
                          channel->LiquidSH,
                          delta_time
@@ -218,12 +257,11 @@ fill_capacities_channel
         "%p liquid cell l %5d r %5d c %5d l %5.2f w %5.2f h %5.2f " \
         "sh %.5e %.5e --> %.5e\n",
         capacities, current_layer, row, column,
-        get_cell_length(dim, column), dim->Cell.Width, channel->Height,
+        get_cell_length(dimensions, column), get_cell_width (dimensions),
+        channel->Height,
         channel->LiquidSH, delta_time, *capacities) ;
 #endif
       }
-    } /* column */
-  } /* row */
 
   return capacities ;
 }
@@ -236,18 +274,18 @@ double *
 fill_sources_channel
 (
 #ifdef DEBUG_FILL_SOURCES
-  FILE *debug,
-  int current_layer,
+  FILE       *debug,
+  int        current_layer,
 #endif
-  Channel *channel,
-  double *sources,
-  Dimensions *dim
+  Channel    *channel,
+  double     *sources,
+  Dimensions *dimensions
 )
 {
   int row, column ;
 
   double C
-    = channel->LiquidSH * 1.62e6 * 0.5 * (dim->Cell.Length * channel->Height ) ;
+    = channel->LiquidSH * 1.62e6 * 0.5 * (dimensions->Cell.Length * channel->Height ) ;
 
 #ifdef DEBUG_FILL_SOURCES
   fprintf (debug,
@@ -255,10 +293,21 @@ fill_sources_channel
     sources, current_layer, channel->WallMaterial->Id, C) ;
 #endif
 
-  for (row = 0 ; row < dim->Grid.NRows ; row++)
-  {
-    for (column = 0 ; column < dim->Grid.NColumns ; column++, sources++)
-    {
+  for
+  (
+    row = 0 ;
+    row < get_number_of_rows (dimensions) ;
+    row++
+  )
+
+    for
+    (
+      column = 0 ;
+      column < get_number_of_columns (dimensions) ;
+      column++,
+      sources++
+    )
+
       if (row == 0 && column % 2 != 0) /* Only first row and odd columns */
       {
         *sources = 2.0 * C * 300.0 ;
@@ -266,13 +315,10 @@ fill_sources_channel
       fprintf (debug,
         "liquid cell l %5d r %5d c %5d (%6d) -> %.5e\n",
         current_layer, row, column,
-        current_layer * (dim->Grid.NColumns * dim->Grid.NRows)
-          + row * dim->Grid.NColumns + column, *sources) ;
+        get_cell_offset_in_stack (dimensions, current_layer, row, column),
+        *sources) ;
 #endif
       }
-
-    }
-  }
 
   return sources ;
 }
@@ -285,19 +331,19 @@ int
 fill_system_matrix_channel
 (
 #ifdef DEBUG_FILL_SYSTEM_MATRIX
-  FILE *debug,
-  Channel *channel,
+  FILE        *debug,
+  Channel     *channel,
 #endif
-  Dimensions *dim,
+  Dimensions  *dimensions,
   Resistances *resistances,
-  double *capacities,
-  int *columns,
-  int *rows,
-  double *values,
-  int current_layer
+  double      *capacities,
+  int         *columns,
+  int         *rows,
+  double      *values,
+  int         current_layer
 )
 {
-  int c_row, c_column, added, tot_added ;
+  int row, column, added, tot_added ;
 
 #ifdef DEBUG_FILL_SYSTEM_MATRIX
   fprintf (debug,
@@ -306,45 +352,54 @@ fill_system_matrix_channel
     current_layer, channel->WallMaterial->Id) ;
 #endif
 
-  for (tot_added = 0 , c_row = 0 ; c_row < dim->Grid.NRows ; c_row++)
-  {
+  for
+  (
+    tot_added = 0 ,
+    row = 0 ;
+    row < get_number_of_rows (dimensions) ;
+    row++
+  )
+
     for
     (
-      c_column = 0 ;
-
-      c_column < dim->Grid.NColumns ;
-
+      column = 0 ;
+      column < get_number_of_columns (dimensions) ;
       resistances ++ ,
       capacities  ++ ,
       columns     ++ ,
       rows        += added ,
       values      += added ,
       tot_added   += added ,
-      c_column    ++
+      column      ++
     )
-    {
-       if (c_column % 2 == 0 ) /* Even -> Wall */
-       {
-         added = add_solid_column (
+
+       if (column % 2 == 0 ) /* Even -> Wall */
+
+         added = add_solid_column
+                 (
 #ifdef DEBUG_FILL_SYSTEM_MATRIX
                    debug,
 #endif
-                   dim, resistances, capacities,
-                   current_layer, c_row, c_column,
-                   columns, rows, values) ;
-       }
+                   dimensions, resistances, capacities,
+                   current_layer, row, column,
+                   columns, rows, values
+                 ) ;
+
        else                  /* Odd -> liquid */
-       {
-         added = add_liquid_column (
+
+         added = add_liquid_column
+                 (
 #ifdef DEBUG_FILL_SYSTEM_MATRIX
                    debug,
 #endif
-                   dim, resistances, capacities,
-                   current_layer, c_row, c_column,
-                   columns, rows, values) ;
-       }
-    } /* column */
-  } /* row */
+                   dimensions, resistances, capacities,
+                   current_layer, row, column,
+                   columns, rows, values
+                 ) ;
 
   return tot_added ;
 }
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
