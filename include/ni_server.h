7b89b47cae3064413fc47f0f21d9a11abeb11dd0
vincenzi <vincenzi@97db0c5e-453e-47ca-9a6c-32dc3a2cf244>
2012-01-09 13:47:57 +0000

    Added (almost) empty files for client/server emulation

---
+++ include/ni_server.h
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * This file is part of 3D-ICE, version 2.0 .                                 *
+ *                                                                            *
+ * 3D-ICE is free software: you can  redistribute it and/or  modify it  under *
+ * the terms of the  GNU General  Public  License as  published by  the  Free *
+ * Software  Foundation, either  version  3  of  the License,  or  any  later *
+ * version.                                                                   *
+ *                                                                            *
+ * 3D-ICE is  distributed  in the hope  that it will  be useful, but  WITHOUT *
+ * ANY  WARRANTY; without  even the  implied warranty  of MERCHANTABILITY  or *
+ * FITNESS  FOR A PARTICULAR  PURPOSE. See the GNU General Public License for *
+ * more details.                                                              *
+ *                                                                            *
+ * You should have  received a copy of  the GNU General  Public License along *
+ * with 3D-ICE. If not, see <http://www.gnu.org/licenses/>.                   *
+ *                                                                            *
+ *                             Copyright (C) 2010                             *
+ *   Embedded Systems Laboratory - Ecole Polytechnique Federale de Lausanne   *
+ *                            All Rights Reserved.                            *
+ *                                                                            *
+ * Authors: Arvind Sridhar                                                    *
+ *          Alessandro Vincenzi                                               *
+ *          Giseong Bak                                                       *
+ *          Martino Ruggiero                                                  *
+ *          Thomas Brunschwiler                                               *
+ *          David Atienza                                                     *
+ *                                                                            *
+ * For any comment, suggestion or request  about 3D-ICE, please  register and *
+ * write to the mailing list (see http://listes.epfl.ch/doc.cgi?liste=3d-ice) *
+ * Any usage  of 3D-ICE  for research,  commercial or other  purposes must be *
+ * properly acknowledged in the resulting products or publications.           *
+ *                                                                            *
+ * EPFL-STI-IEL-ESL                                                           *
+ * Batiment ELG, ELG 130                Mail : 3d-ice@listes.epfl.ch          *
+ * Station 11                                  (SUBSCRIPTION IS NECESSARY)    *
+ * 1015 Lausanne, Switzerland           Url  : http://esl.epfl.ch/3d-ice.html *
+ ******************************************************************************/
+
+#ifndef _3DICE_SERVER_H_
+#define _3DICE_SERVER_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/******************************************************************************/
+
+#include "types.h"
+
+/******************************************************************************/
+
+    int init_server_unix_domain (int *socket_id, String_t socket_name) ;
+
+    int init_server_internet_domain (int *server_socket_id, int port_number) ;
+
+    void close_socket (int socket_id) ;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _3DICE_SERVER_H_ */
