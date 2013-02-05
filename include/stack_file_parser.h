68671860ec79ed82195c3df6edbf2d8d3497d4b2
vincenzi <vincenzi@97db0c5e-453e-47ca-9a6c-32dc3a2cf244>
2013-02-05 09:56:18 +0000

    Made tag 2.2.4 (to release) out of branch 2.2-revision@880

--- include/stack_file_parser.h
+++ include/stack_file_parser.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * This file is part of 3D-ICE, version 2.2.3 .                               *
+ * This file is part of 3D-ICE, version 2.2.4 .                               *
  *                                                                            *
  * 3D-ICE is free software: you can  redistribute it and/or  modify it  under *
  * the terms of the  GNU General  Public  License as  published by  the  Free *
